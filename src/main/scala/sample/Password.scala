da8f25d386a4c11b1c752ce6682cdde3b1659765
toshinori.chiba <toshinori.chiba.p@gmail.com>
2021-12-12 15:55:52 +0900

    コメント追加

--- src/main/scala/sample/Password.scala
+++ src/main/scala/sample/Password.scala
@@ -3,7 +3,20 @@ package sample
 import java.io.{Externalizable, ObjectInput, ObjectOutput}
 import scala.util.matching.Regex
 
-final class Password private (@transient private var underlying: String) extends Externalizable {
+/**
+ * パスワードクラス
+ *
+ * [[https://book.mynavi.jp/ec/products/detail/id=124056 セキュア・バイ・デザイン]]
+ * のRead-Onceオブジェクトところを読んで試しにScalaで作成。
+ *
+ * @note 章を参考に同じような実装をしてみたが、プレーンパスワードのvalueを外に出すのではなく、
+ *       ハッシュ化する処理も入れて、プレーンパスワードは内部だけで扱う方が安全なような気もする。
+ *       JVMのメモリに残らないようにといった話もあるので、よくわからないが。。
+ * @param underlying プレーンパスワード
+ */
+final class Password private (                // 継承させない
+    @transient private var underlying: String // シリアライズ不可
+) extends Externalizable { // シリアライズが行われる際の振る舞いを制御
 
   import Password._
 
@@ -11,21 +24,21 @@ final class Password private (@transient private var underlying: String) extends
 
   private var consumed = false
 
-  def value: String = synchronized {
+  def value: String = synchronized { // 1度に1スレッドからしかアクセスできない
     if (consumed) throw new IllegalStateException("Password value has already been consumed")
 
     val returnValue = underlying
-    consumed = true
-    underlying = ""
+    consumed = true // 読み込まれたことを伝えるフラグ
+    underlying = "" // 保持していた値を削除
 
     returnValue
   }
 
-  override def writeExternal(out: ObjectOutput): Unit = deny
+  override def writeExternal(out: ObjectOutput): Unit = deny // シリアライズされそうになると、例外をスローする
 
-  override def readExternal(in: ObjectInput): Unit = deny
+  override def readExternal(in: ObjectInput): Unit = deny // シリアライズされそうになると例外をスローする
 
-  override def toString = s"Password(underlying=********)"
+  override def toString = s"Password(underlying=********)" // 意図しない漏洩を防ぐ
 }
 
 object Password {
